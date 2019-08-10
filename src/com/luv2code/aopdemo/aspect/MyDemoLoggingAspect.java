9067fe062c4ad6cce313771f44d8e55d4eb50034
Jupraj Singh <juprajsingh1@gmail.com>
2019-08-10 03:00:03 -0400

    added a pointcut combo called forDaoPackageNoGetterSetter()

--- src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
+++ src/com/luv2code/aopdemo/aspect/MyDemoLoggingAspect.java
@@ -13,14 +13,28 @@ public class MyDemoLoggingAspect {
 	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
-	@Before("forDaoPackage()")
+	// create another point cut for getter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.get*(..))")
+	private void getter() {}
+	
+	// create pointcut for setter methods
+	@Pointcut("execution(* com.luv2code.aopdemo.dao.*.set*(..))")
+	private void setter() {}
+	
+	// create point: include package ... exclude getter/setter
+	@Pointcut("forDaoPackage() && !(getter() || setter())")
+	private void forDaoPackageNoGetterSetter() {}
+	
+	@Before("forDaoPackageNoGetterSetter()")
 	public void beforeAddAccountAdvice() {
 		System.out.println("\n======>>> Executing @Before advice on updateAccount()");
 	}
 	
-	@Before("forDaoPackage()")
+	@Before("forDaoPackageNoGetterSetter()")
 	public void performApiAnalytics() {
 		System.out.println("\n======>>> Performing API analyytics");
-		
 	}
+	
+	
+	
 }
--- src/com/luv2code/aopdemo/dao/AccountDAO.java
+++ src/com/luv2code/aopdemo/dao/AccountDAO.java
@@ -7,20 +7,40 @@ import com.luv2code.aopdemo.Account;
 @Component
 public class AccountDAO {
 
-	public void addAccount(Account theAccount, boolean vipFlag) {
-		
-		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
 	
+	private String name;
+	private String serviceCode;
+	
+	public void addAccount(Account theAccount, boolean vipFlag) {
+		System.out.println(getClass() + ": DOING MY DB WORK: ADDING AN ACCOUNT");
 	}
 
 	public boolean doWork() {
-		
 		System.out.println(getClass() + ": doWork()");
-		
 		return false;	
 	}
 
+	public String getName() {
+		System.out.println(getClass() + ": in getName()");
+		return name;
+	}
+
+	public void setName(String name) {
+		System.out.println(getClass() + ": in setName()");
+		this.name = name;
+	}
+
+	public String getServiceCode() {
+		System.out.println(getClass() + ": in getServiceCode()");
+		return serviceCode;
+	}
+
+	public void setServiceCode(String serviceCode) {
+		System.out.println(getClass() + ": in setServiceCode()");
+		this.serviceCode = serviceCode;
+	}
 
 
+	
 
 }
